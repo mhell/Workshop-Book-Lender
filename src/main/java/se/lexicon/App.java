208ef8ab1a7992b92ae8c3722b8a9522ef90eeda
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-30 16:33:24 +0100

    Update printout

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -5,14 +5,12 @@ import se.lexicon.model.*;
 public class App {
     public static void main(String[] args) {
         // Create a book instance
-        Book book = new Book("A Book Title", "An Author");
-
+        Book book = new Book("Pride and Prejudice", "Jane Austin");
         // Display book information
         System.out.println(book.getBookInformation());
 
         // Create a person instance
         Person person = new Person("Nisse", "Nilsson");
-
         // Display person information
         System.out.println(person.getPersonInformation());
 
@@ -20,23 +18,19 @@ public class App {
 
         // Loan a book to the person
         person.loanBook(book);
-
-        // Display person information after borrowing a book
-        System.out.println(person.getPersonInformation());
-
         // Display book information after borrowing a book
         System.out.println(book.getBookInformation());
+        // Display person information after borrowing a book
+        System.out.println(person.getPersonInformation());
 
         System.out.println("Return a book:");
 
         // Return the borrowed book
         person.returnBook(book);
-
-        // Display person information after returning the book
-        System.out.println(person.getPersonInformation());
-
         // Display book information after borrowing a book
         System.out.println(book.getBookInformation());
+        // Display person information after returning the book
+        System.out.println(person.getPersonInformation());
 
     }
 

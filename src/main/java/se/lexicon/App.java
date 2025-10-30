5ad395a24f002137678effe13228813a45f7f516
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-30 15:08:23 +0100

    Implement App

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,25 +1,42 @@
 package se.lexicon;
 
+import se.lexicon.model.*;
 
 public class App {
     public static void main(String[] args) {
-        // todo: needs completion
-
         // Create a book instance
+        Book book = new Book("A Book Title", "An Author");
+
         // Display book information
+        System.out.println(book.getBookInformation());
 
         // Create a person instance
+        Person person = new Person("Nisse", "Nilsson");
+
         // Display person information
+        System.out.println(person.getPersonInformation());
+
+        System.out.println("Loan a book:");
 
         // Loan a book to the person
+        person.loanBook(book);
 
         // Display person information after borrowing a book
+        System.out.println(person.getPersonInformation());
+
         // Display book information after borrowing a book
+        System.out.println(book.getBookInformation());
+
+        System.out.println("Return a book:");
 
         // Return the borrowed book
+        person.returnBook(book);
 
         // Display person information after returning the book
+        System.out.println(person.getPersonInformation());
+
         // Display book information after borrowing a book
+        System.out.println(book.getBookInformation());
 
     }
 

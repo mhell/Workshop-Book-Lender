0181df93da2fecbb4e2b54adadda1380edfbd062
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-30 14:02:54 +0100

    Update Person

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,15 +1,13 @@
 package se.lexicon.model;
 
-import java.rmi.server.UID;
-import java.util.List;
-import java.util.UUID;
+import java.util.Arrays;
 
 public class Person {
     private static int sequencer = 0;
     private final int id;
     private String firstName;
     private String lastName;
-    private Book[] books = new Book[0];
+    private Book[] borrowed = new Book[0];
 
     public Person(String firstName, String lastName) {
         setFirstName(firstName);
@@ -41,19 +39,52 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public Book[] getBooks() {
-        return books;
+    public Book[] getBorrowed() {
+        return borrowed;
     }
 
-    private void setBooks(Book[] books) {
-        this.books = books;
+    private void setBorrowed(Book[] borrowed) {
+        this.borrowed = borrowed;
     }
 
-    public void loanBook() {
+    public void loanBook(Book book) {
+        // Check if book is available
+        if (book.isAvailable()) {
+            // Add person as the books borrower
+            book.setBorrower(this);
+            // Add book to persons list of borrowed books
+            Book[] newBorrowed = Arrays.copyOf(borrowed, borrowed.length + 1);
+            newBorrowed[newBorrowed.length-1] = book;
+            setBorrowed(newBorrowed);
+        } else {
+            throw new RuntimeException("The book is not available");
+        }
+    }
 
+    public void returnBook (Book book) {
+        Book[] newBorrowed = new Book[borrowed.length];
+        // create a new array without this book
+        for (int i=0, j=0; i < borrowed.length; i++) {
+            // copy all elements from borrowed, except if element.id == book.id
+            if (!borrowed[i].getId().equals(book.getId())) {
+                newBorrowed[j] = borrowed[i];
+                j++;
+            }
+        }
+        // if successfully removed the book
+        if (newBorrowed.length < borrowed.length) {
+            // remove borrower from the book (sets it to available)
+            book.setBorrower(null);
+            // reassign borrowed array
+            borrowed = newBorrowed;
+        // else throw an exception (the book was never borrowed)
+        } else {
+            throw new RuntimeException("Book was not borrowed");
+        }
     }
 
     public String getPersonInformation() {
-        return "";
+        return String.format("First name: %s, last name: %s, borrowed: %s",
+                getFirstName(), getLastName(), Arrays.toString(borrowed));
     }
 }

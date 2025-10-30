91b93397b2df34038b937a2e97381f4e7dac7046
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-30 17:01:28 +0100

    Use toString methods

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -7,31 +7,30 @@ public class App {
         // Create a book instance
         Book book = new Book("Pride and Prejudice", "Jane Austin");
         // Display book information
-        System.out.println(book.getBookInformation());
+        System.out.println(book);
 
         // Create a person instance
         Person person = new Person("Nisse", "Nilsson");
         // Display person information
-        System.out.println(person.getPersonInformation());
+        System.out.println(person);
 
         System.out.println("Loan a book:");
 
         // Loan a book to the person
         person.loanBook(book);
         // Display book information after borrowing a book
-        System.out.println(book.getBookInformation());
+        System.out.println(book);
         // Display person information after borrowing a book
-        System.out.println(person.getPersonInformation());
+        System.out.println(person);
 
         System.out.println("Return a book:");
 
         // Return the borrowed book
         person.returnBook(book);
         // Display book information after borrowing a book
-        System.out.println(book.getBookInformation());
+        System.out.println(book);
         // Display person information after returning the book
-        System.out.println(person.getPersonInformation());
-
+        System.out.println(person);
     }
 
 }

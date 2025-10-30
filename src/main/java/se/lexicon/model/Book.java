352092b5d3d79106e7886a7cbe52c6dd8a88190c
Mattias Hellman <mattias.hellman@gmail.com>
2025-10-30 10:36:47 +0100

    Implement Book

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,5 +1,60 @@
 package se.lexicon.model;
 
+import java.util.UUID;
+
 public class Book {
-    // todo: needs completion
+    private final String id;
+    private String title;
+    private String author;
+    private boolean available;
+    private Person borrower;
+
+    public Book(String title, String author) {
+        setTitle(title);
+        setAuthor(author);
+        setAvailable(true);
+        id = UUID.randomUUID().toString();
+    }
+
+    public Book(String title, String author, Person borrower) {
+        this(title, author);
+        setBorrower(borrower);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    private void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Person getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Person borrower) {
+        this.borrower = borrower;
+        setAvailable(false);
+    }
 }
